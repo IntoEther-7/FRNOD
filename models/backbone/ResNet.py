b2897328a42f4c481706f798954b796437e187d7
Ether <1779562376@qq.com>
2022-11-14 09:35:41 +0800

    SFNet引入

--- models/backbone/ResNet.py
+++ models/backbone/ResNet.py
@@ -7,7 +7,11 @@ from typing import Type, List, Union, Optional, Callable, Any
 import torch
 from torch import nn, Tensor
 from torch.hub import load_state_dict_from_url
+from torchvision.models import resnet
+from torchvision.models.detection.backbone_utils import BackboneWithFPN
 from torchvision.models.resnet import BasicBlock, Bottleneck, conv1x1, model_urls
+from torchvision.ops import misc
+from torchvision.ops.feature_pyramid_network import LastLevelMaxPool
 
 
 class ResNet(nn.Module):
@@ -163,12 +167,84 @@ def resnet50(pretrained: bool = False, progress: bool = True, frozen: bool = Fal
         progress (bool): If True, displays a progress bar of the download to stderr
     """
     model = _resnet('resnet50', Bottleneck, [3, 4, 6, 3], pretrained, progress, frozen,
-                   **kwargs)
+                    **kwargs)
     if frozen:
         for p in model.parameters():
             p.requires_grad = False
     model.s_scale = 32
     model.out_channels = 2048
+    return model
+
+
+def resnet_fpn_backbone(
+        backbone_name,
+        pretrained,
+        norm_layer=misc.FrozenBatchNorm2d,
+        trainable_layers=3,
+        returned_layers=None,
+        extra_blocks=None
+):
+    """
+    Constructs a specified ResNet backbone with FPN on top. Freezes the specified number of layers in the backbone.
+
+    Examples::
+
+        >>> from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+        >>> backbone = resnet_fpn_backbone('resnet50', pretrained=True, trainable_layers=3)
+        >>> # get some dummy image
+        >>> x = torch.rand(1,3,64,64)
+        >>> # compute the output
+        >>> output = backbone(x)
+        >>> print([(k, v.shape) for k, v in output.items()])
+        >>> # returns
+        >>>   [('0', torch.Size([1, 256, 16, 16])),
+        >>>    ('1', torch.Size([1, 256, 8, 8])),
+        >>>    ('2', torch.Size([1, 256, 4, 4])),
+        >>>    ('3', torch.Size([1, 256, 2, 2])),
+        >>>    ('pool', torch.Size([1, 256, 1, 1]))]
+
+    Args:
+        backbone_name (string): resnet architecture. Possible values are 'ResNet', 'resnet18', 'resnet34', 'resnet50',
+             'resnet101', 'resnet152', 'resnext50_32x4d', 'resnext101_32x8d', 'wide_resnet50_2', 'wide_resnet101_2'
+        pretrained (bool): If True, returns a model with backbone pre-trained on Imagenet
+        norm_layer (torchvision.ops): it is recommended to use the default value. For details visit:
+            (https://github.com/facebookresearch/maskrcnn-benchmark/issues/267)
+        trainable_layers (int): number of trainable (not frozen) resnet layers starting from final block.
+            Valid values are between 0 and 5, with 5 meaning all backbone layers are trainable.
+        returned_layers (list of int): The layers of the network to return. Each entry must be in ``[1, 4]``.
+            By default all layers are returned.
+        extra_blocks (ExtraFPNBlock or None): if provided, extra operations will
+            be performed. It is expected to take the fpn features, the original
+            features and the names of the original features as input, and returns
+            a new list of feature maps and their corresponding names. By
+            default a ``LastLevelMaxPool`` is used.
+    """
+    backbone = resnet.__dict__[backbone_name](
+        pretrained=pretrained,
+        norm_layer=norm_layer)
+
+    # select layers that wont be frozen
+    assert 0 <= trainable_layers <= 5
+    layers_to_train = ['layer4', 'layer3', 'layer2', 'layer1', 'conv1'][:trainable_layers]
+    if trainable_layers == 5:
+        layers_to_train.append('bn1')
+    for name, parameter in backbone.named_parameters():
+        if all([not name.startswith(layer) for layer in layers_to_train]):
+            parameter.requires_grad_(False)
+
+    if extra_blocks is None:
+        extra_blocks = LastLevelMaxPool()
+
+    if returned_layers is None:
+        returned_layers = [1, 2, 3, 4]
+    assert min(returned_layers) > 0 and max(returned_layers) < 5
+    return_layers = {f'layer{k}': str(v) for v, k in enumerate(returned_layers)}
+
+    in_channels_stage2 = backbone.inplanes // 8
+    in_channels_list = [in_channels_stage2 * 2 ** (i - 1) for i in returned_layers]
+    out_channels = 256
+    model = BackboneWithFPN(backbone, return_layers, in_channels_list, out_channels, extra_blocks=extra_blocks)
+    model.s_scale = 64
     return model
 
 
